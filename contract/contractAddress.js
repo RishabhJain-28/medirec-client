0ed316b196774bb503f3de26848bdd279cff6e5a
Rishabh Jain <rjain_be19@thapar.edu>
2022-09-13 01:06:41 +0530

    sab chal rhaaaaaaa

--- contract/contractAddress.js
+++ contract/contractAddress.js
@@ -1,2 +1,2 @@
 // export const patientAddress = "0xf05abe74932D9e781c537eD8859Aa4CAeb791B2d"
-export const patientAddress = "0xE80680a115C8E9BCB048eC0260cD60eabE1e282e";
+export const patientAddress = "0x62D21e13b1E85595e31FEc721300b71dD8cc4856";
